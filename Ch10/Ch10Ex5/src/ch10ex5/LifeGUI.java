3b9b4ec6706dee12a2537e99b18539f89f395829
sestinj <33237525+sestinj@users.noreply.github.com>
2018-11-02 12:11:03 -0400

    Update LifeGUI.java

--- Ch10/Ch10Ex5/src/ch10ex5/LifeGUI.java
+++ Ch10/Ch10Ex5/src/ch10ex5/LifeGUI.java
@@ -16,19 +16,28 @@ public class LifeGUI {
     private JFrame frame;
     private JPanel panel;
     private JButton[][] buttons = new JButton[20][20];
+
+    private JButton next;
     
     public LifeGUI() {
         frame = new JFrame("Life");
         panel = new JPanel();
         frame.setContentPane(panel);
         
-        for (int i = 0;i<buttons.length;i++)) {
-            for (JButton button : buttonsList) {
-                button = new JButton("0");
+        for (int i = 0;i<buttons.length;i++) {
+            for (int j = 0; j<buttons[i].length;j++) {
+                buttons[i][j] = new JButton("0");
+                buttons[i][j].setActionCommand(i+”,”+j);
+buttons[i][j].addEventListener(this);
+panel.add(buttons[i][j]);
                 button.setActionCommand(actionCommand);
             }
         }
         
+        next = new JButton(“Next”);
+        next.setActionCommand(“next”);
+next.addEventListener(this);
+panel.add(next);
         
         frame.pack();
         frame.setVisible(true);
